099126a06a53880c4db2c7a37f014cfdf8742540
luanalm <98362897+luanalm@users.noreply.github.com>
2023-08-27 22:03:49 -0300

    Exclusão da pasta Aula12 e inclusão da Aula13

---
+++ LaçosDeRepetição/aula13.py
@@ -0,0 +1,10 @@
+
+tab = int(input('Escolha o número da tabuáda desejada: '))
+
+for x in range(0,11):
+    y = tab * x
+    print('{} x {} = {}'.format(tab, x, y))
+
+# Repetição de somas
+# s = s + n
+# s += n
